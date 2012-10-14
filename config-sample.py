0f0d49de511517b289f99655f8417031a8c7c453
Andrea Peltrin <andrea@passiomatic.com>
2012-10-14 16:36:19 +0300

    Fixed RSS demo URL with new Twitter feed structure

--- config-sample.py
+++ config-sample.py
@@ -6,6 +6,6 @@ FEEDS = [
     # type (BLOG|TWITTER|DELICIOUS), site, feed url and flavor (atom|rss)
 #    (BLOG, 'http://someblog.com/', 'http://someblog.com/feed/', 'atom'),
 #    (DELICIOUS, 'http://delicious.com/username', 'http://feeds.delicious.com/v2/rss/username?count=15', 'rss'),
-    (TWITTER, 'http://twitter.com/passiomatc', 'http://twitter.com/statuses/user_timeline/14973295.atom', 'atom'),
+    (TWITTER, 'http://twitter.com/passiomatic', 'https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=passiomatic', 'rss'),
 ]
 
